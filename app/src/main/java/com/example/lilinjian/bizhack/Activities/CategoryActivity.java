d3d3abf4c2ee017c744567f1fd84b81b9267c1e1
Forrest <lljsg123123@gmail.com>
2018-03-23 23:52:49 -0700

    Add packages

--- app/src/main/java/com/example/lilinjian/bizhack/Activities/CategoryActivity.java
+++ app/src/main/java/com/example/lilinjian/bizhack/Activities/CategoryActivity.java
@@ -1,13 +1,15 @@
-package com.example.lilinjian.bizhack;
+package com.example.lilinjian.bizhack.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.lilinjian.bizhack.R;
+
+public class CategoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_category);
     }
 }
---
+++ app/src/main/java/com/example/lilinjian/bizhack/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lilinjian.bizhack.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lilinjian.bizhack.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
