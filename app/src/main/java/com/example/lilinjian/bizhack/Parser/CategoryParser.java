8f802e376a1b89697a7859f52cb297d45f75feb7
Forrest <lljsg123123@gmail.com>
2018-03-24 01:08:35 -0700

    add parsers

---
+++ app/src/main/java/com/example/lilinjian/bizhack/Parser/CategoryParser.java
@@ -0,0 +1,8 @@
+package com.example.lilinjian.bizhack.Parser;
+
+/**
+ * Created by lilinjian on 24/03/2018.
+ */
+
+public class CategoryParser {
+}
--- app/src/main/java/com/example/lilinjian/bizhack/Parser/SearchParser.java
+++ app/src/main/java/com/example/lilinjian/bizhack/Parser/SearchParser.java
@@ -4,5 +4,5 @@ package com.example.lilinjian.bizhack.Parser;
  * Created by lilinjian on 24/03/2018.
  */
 
-public class hahah {
+public class SearchParser {
 }
